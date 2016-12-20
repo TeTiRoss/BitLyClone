e3e247a13089cf5b33a9be98bfd4f3ea874005b3
TeTiRoss <rostyk.s@hmarynka.com>
2016-12-20 16:48:43 +0200

    ability to add link and see result page

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,11 +1,32 @@
 class LinksController < ApplicationController
+  before_action :set_link, only: [:show, :shortened]
+
+
   def new
     @link = Link.new
   end
 
   def create
+    @link = Link.new(path: params[:link][:path])
+    @link.name = 'ShortenLink'
+    if @link.save
+      redirect_to shortened_links_path(id: @link.id)
+    else
+      flash.now.alert = 'Not valid link'
+      render :new
+    end
   end
 
   def show
+    redirect_to "http://#{link.path}"
+  end
+
+  def shortened
+  end
+
+  private
+
+  def set_link
+    @link = Link.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :links, only: [:new, :create, :show]
-  
+  resources :links, only: [:new, :create, :show] do
+    get 'shortened', on: :collection
+  end
+
   root 'links#new'
 end

26ed3a01fce38b36f9ad3dd3baa1f4133e6212b5
TeTiRoss <rostyk.s@hmarynka.com>
2016-12-20 16:56:49 +0200

    link path validation

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,6 @@
 class Link < ApplicationRecord
   validates :name, :path, presence: true
+  validates_format_of :path, with: %r{
+    /\A(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)*\/?\Z/i
+  }x
 end

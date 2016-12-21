cade32382355cf90f94c0ba64066050400659cb9
TeTiRoss <rostyk.s@hmarynka.com>
2016-12-21 19:48:27 +0200

    concern path fixed

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,6 +1,6 @@
 class Link < ApplicationRecord
-  include LinkName
-  include LinkPath
+  include Link::LinkName
+  include Link::LinkPath
 
   validates :path, presence: true
   validates_format_of :path, with: /\A(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)*\/?\Z/i

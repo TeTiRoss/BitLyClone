15828fb585e15cd195b246c3cac818129d4fe249
TeTiRoss <rostyk.s@hmarynka.com>
2016-12-21 18:43:08 +0200

    fixed specs

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Link, type: :model do
-  it { should validate_presence_of(:name) }
   it { should validate_presence_of(:path) }
   it { should allow_value('http://foo.com', 'football.ua').for(:path) }
   it { should_not allow_value('foocom', 'hht://football.ua').for(:path) }
